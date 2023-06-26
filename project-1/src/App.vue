321a08af2968e2dafe4dc98c06d199ca5285f432
Breno Viana <brenomfviana@gmail.com>
2023-06-26 17:21:49 -0300

    adds sidebar and form

---
+++ project-1/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+    <div class="column is-one-quarter">
+      <Sidebar />
+    </div>
+    <div class="column is-three-quarter">
+      <Form />
+    </div>
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Sidebar from "./components/Sidebar.vue";
+import Form from "./components/Form.vue";
+
+export default defineComponent({
+  name: "App",
+  components: {
+    Sidebar,
+    Form,
+  },
+});
+</script>
+
+<style scoped></style>
---
+++ project-1/src/components/Form.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="box">
+    <div class="columns">
+      <div class="column is-8" role="form" aria-label="New task form">
+        <input
+          type="text"
+          class="input"
+          placeholder="Which task do you want to start?"
+        />
+      </div>
+      <div class="column">
+        <div
+          class="is-flex is-align-items-center is-justify-content-space-between"
+        >
+          <section>
+            <strong>{{ elapsedTime }}</strong>
+          </section>
+          <button class="button" @click="start">
+            <span class="icon">
+              <i class="fas fa-play"></i>
+            </span>
+            <span>play</span>
+          </button>
+          <button class="button" @click="finish">
+            <span class="icon">
+              <i class="fas fa-stop"></i>
+            </span>
+            <span>stop</span>
+          </button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "Form",
+  data() {
+    return {
+      timeInSeconds: 0,
+      chronometer: 0,
+    };
+  },
+  computed: {
+    elapsedTime(): string {
+      return new Date(this.timeInSeconds * 1000)
+        .toISOString()
+        .substring(11, 11 + 8);
+    },
+  },
+  methods: {
+    start() {
+      this.chronometer = setInterval(() => {
+        this.timeInSeconds += 1;
+      }, 1000);
+      console.log("starting...");
+    },
+    finish() {
+      clearInterval(this.chronometer);
+      console.log("finishing...");
+    },
+  },
+});
+</script>
