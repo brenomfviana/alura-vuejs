61b2e3a28a0decca3da1d729cf01425877a62ed8
Breno Viana <brenomfviana@gmail.com>
2023-06-27 11:08:38 -0300

    adds timer component

--- project-1/src/components/Form.vue
+++ project-1/src/components/Form.vue
@@ -9,23 +9,7 @@
         />
       </div>
       <div class="column">
-        <div
-          class="is-flex is-align-items-center is-justify-content-space-between"
-        >
-          <Chronometer :timeInSeconds="timeInSeconds" />
-          <button class="button" @click="start">
-            <span class="icon">
-              <i class="fas fa-play"></i>
-            </span>
-            <span>play</span>
-          </button>
-          <button class="button" @click="finish">
-            <span class="icon">
-              <i class="fas fa-stop"></i>
-            </span>
-            <span>stop</span>
-          </button>
-        </div>
+        <Timer />
       </div>
     </div>
   </div>
@@ -33,30 +17,12 @@
 
 <script lang="ts">
 import { defineComponent } from "vue";
-import Chronometer from "./Chronometer.vue";
+import Timer from "./Timer.vue";
 
 export default defineComponent({
   name: "Form",
   components: {
-    Chronometer,
-  },
-  data() {
-    return {
-      timeInSeconds: 0,
-      chronometer: 0,
-    };
-  },
-  methods: {
-    start() {
-      this.chronometer = setInterval(() => {
-        this.timeInSeconds += 1;
-      }, 1000);
-      console.log("starting...");
-    },
-    finish() {
-      clearInterval(this.chronometer);
-      console.log("finishing...");
-    },
+    Timer,
   },
 });
 </script>
---
+++ project-1/src/components/Timer.vue
@@ -0,0 +1,50 @@
+<template>
+  <div class="is-flex is-align-items-center is-justify-content-space-between">
+    <Chronometer :timeInSeconds="timeInSeconds" />
+    <button class="button" @click="start" :disabled="isRunning">
+      <span class="icon">
+        <i class="fas fa-play"></i>
+      </span>
+      <span>play</span>
+    </button>
+    <button class="button" @click="finish" :disabled="!isRunning">
+      <span class="icon">
+        <i class="fas fa-stop"></i>
+      </span>
+      <span>stop</span>
+    </button>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+import Chronometer from "./Chronometer.vue";
+
+export default defineComponent({
+  name: "Timer",
+  components: {
+    Chronometer,
+  },
+  data() {
+    return {
+      timeInSeconds: 0,
+      chronometer: 0,
+      isRunning: false,
+    };
+  },
+  methods: {
+    start() {
+      this.isRunning = true;
+      this.chronometer = setInterval(() => {
+        this.timeInSeconds += 1;
+      }, 1000);
+      console.log("starting...");
+    },
+    finish() {
+      this.isRunning = false;
+      clearInterval(this.chronometer);
+      console.log("finishing...");
+    },
+  },
+});
+</script>
