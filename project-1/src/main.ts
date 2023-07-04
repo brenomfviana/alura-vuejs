7124e913d95e87fd20990bd9067d7fe0189cd735
Breno Viana <brenomfviana@gmail.com>
2023-07-04 16:01:40 -0300

    fixes font awesome usage

--- project-1/src/main.ts
+++ project-1/src/main.ts
@@ -1,5 +1,6 @@
 import { createApp } from "vue";
 import App from "./App.vue";
+import "@fortawesome/fontawesome-free/css/all.css";
 import router from "./router";
 
 const app = createApp(App).use(router).mount("#app");
